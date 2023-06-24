87bcc9e937856631ab728367354d34b2262da1ee
Abdulrazzak Alsssouki <abdulrzakalsssouki@gmail.com>
2023-06-24 12:50:07 +0200

    switch back to envoriment variable

--- Week3/transactions/connection.js
+++ Week3/transactions/connection.js
@@ -3,14 +3,10 @@ const mysql = require("mysql");
 
 //Database creditnal
 const connection = mysql.createConnection({
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_DATABASE_1,
-  host: "localhost",
-  user: "hyfuser'@'localhost",
-  password: "hyfpassword",
-  database: "transaction",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE_1,
 });
 
 //Connect to the Database
